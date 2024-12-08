8611a483130ef8b6836ab478744166ed45b58c3d
Maksym Zyat <maxzyat03@gmail.com>
2024-12-08 21:41:23 +0200

    learn

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+// src/components/App.jsx
+
+import { Alert } from './Alert.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    </>
+  );
+}
